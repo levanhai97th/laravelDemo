48625786a0155a1fa0bf03f268f14c55811ff1d3
Hai Le Van <hai.le.connectiv@gmail.com>
2021-12-19 14:52:21 +0700

    init

---
+++ src/DemoServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Hailv\Demo\Providers;
+
+use Illuminate\Support\ServiceProvider;
+// use Unleash\Client\UnleashBuilder;
+
+class DemoServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap the application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+    }
+ 
+    /**
+     * Register the application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+ 
+    }
+}
